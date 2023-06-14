5e8ded9978c5befef202679da3306045eb7fae75
Lari <88565631+romugaboo@users.noreply.github.com>
2023-06-14 20:45:59 -0300

    menu adicionado, saídas estão em txt e DFS agora é iterativo

--- grafo.py
+++ grafo.py
@@ -1,45 +1,21 @@
 # Algoritmo de Prim para árvore geradora mínima (MST) por meio da matriz de adjacência
 import sys
+from collections import deque
+import grafoUtils
 
 class Grafo():
 	# Construtor: Monta matriz de adjacência preenchida com zeros
 	def __init__(self, vertices):
-		self.V = vertices
-		self.grafo = [[0 for coluna in range(self.V)]
+		self.v = vertices
+		self.matrizAdj = [[0 for coluna in range(self.v)]
 					for linha in range(vertices)]
 		self.arestas=0
-	# Função que faz a leitura de um grafo em txt e o insere em uma matriz de adjacência
-	def lerGrafo(self, arquivo):
-		with open(arquivo, 'r') as f:
-			linhas = f.readlines()
-		# Lê o número de vértices
-		vertices = int(linhas[0].strip())
-		# Cria matriz de adjacência para o grafo no txt
-		matriz_adjacencia = [[0] * vertices for _ in range(vertices)]
-		for linha in linhas[1:]:
-			valores = linha.strip().split()
-			origem, destino, peso = int(valores[0]), int(valores[1]), int(valores[2])
-			matriz_adjacencia[origem - 1][destino - 1] = peso
-			matriz_adjacencia[destino - 1][origem - 1] = peso
-		
-		# Cria um grafo com as informações de grafo.txt e executa o algoritmo de Prim
-		g = Grafo(vertices)
-		g.grafo = matriz_adjacencia
-		g.arestas=(len(linhas)-1)
-		return g
-
-	# Função que imprime a árvore geradora mínima
-	def printMST(self, pai):
-		print("\nA árvore geradora mínima pelo algoritmo de Prim desse grafo é: \n")
-		print("Aresta \t\tPeso")
-		for i in range(1, self.V):
-			print(pai[i] + 1, "-", i + 1, "\t\t", self.grafo[i][pai[i]])
-
+	
 	# Função que encontra o vértice não visitado de menor peso no grafo
 	# O sys.maxsize é equivalente ao infinito
 	def chaveMin(self, chave, mst):
 		min = sys.maxsize
-		for v in range(self.V):
+		for v in range(self.v):
 			# Se a chave for menor que o valor mínimo e ainda não estiver na MST, atualize a chave
 			if chave[v] < min and mst[v] == False:
 				min = chave[v]
@@ -49,19 +25,19 @@ class Grafo():
 	# Função que constrói a MST
 	def prim(self):
 		# Array de vértices com pesos mínimos
-		chave = [sys.maxsize] * self.V
+		chave = [sys.maxsize] * self.v
 		
 		# Array dos pais preenchido com zeros
-		pai = [None] * self.V 
+		pai = [None] * self.v 
 		
 		# O vértice 0 é escolhido como o primeiro
 		chave[0] = 0
-		mst = [False] * self.V
+		mst = [False] * self.v
 		
 		# O primeiro vértice não tem pai
 		pai[0] = -1
 
-		for i in range(self.V): 
+		for i in range(self.v): 
 			u = self.chaveMin(chave, mst)
 			
 			# Insere menor vértice na lista do novo grafo
@@ -69,95 +45,130 @@ class Grafo():
 			
 			# Se o vértice for vizinho da chave, não está incluso na MST e o peso é o menor dentre todos os vizinhos
 			# Atualiza o valor da chave e do pai
-			for v in range(self.V): 
-				if self.grafo[u][v] > 0 and mst[v] == False and chave[v] > self.grafo[u][v]:
-					chave[v] = self.grafo[u][v]
+			for v in range(self.v): 
+				if self.matrizAdj[u][v] > 0 and mst[v] == False and chave[v] > self.matrizAdj[u][v]:
+					chave[v] = self.matrizAdj[u][v]
 					pai[v] = u
-		self.printMST(pai)
+		grafoUtils.printMST(g, arquivoSaida, pai)
 
 	# Função que faz o BFS
 	def BFS(self, começo):
-		visitado = [False] * self.V
+		visitado = [False] * self.v
 		fila = [começo - 1]
  
 		# Começo já está visitado
-		visitado[começo - 1] = True
-
-		print("\nA árvore de busca pelo BFS é:")
- 
+		visitado[começo - 1] = True 
 		while fila:
 			vis = fila[0]
- 
-			# Imprime nó atual
-			print(vis + 1, end = ' ')
+
+			grafoUtils.printBFS(g, arquivoSaida, vis)
 			fila.pop(0)
 			 
-			# Para todo vértice adjascente do vértice atual
-			for i in range(self.V):
-				if (self.grafo[vis][i] > 1 and (not visitado[i])):
+			# Para todo vértice adjacente do vértice atual
+			for i in range(self.v):
+				if (self.matrizAdj[vis][i] > 1 and (not visitado[i])):
 					# Coloque-o na fila
 					fila.append(i)
 					visitado[i] = True
-		
-	def DFSRecursivo(self, começo, visitado):
-		# Print current node
-		print(começo + 1, end = ' ')
- 
-		# Set current node as visited
-		visitado[começo] = True
- 
-		# For every node of the graph
-		for i in range(self.V):			 
-			# If some node is adjacent to the current node and it has not already been visited
-			if (self.grafo[começo][i] > 1 and (not visitado[i])):
-				self.DFSRecursivo(i, visitado)
 
-	def DFS(self, começo):
-			print("\n\nA árvore de busca pelo DFS é:")
-			visitado = [False] * g.V
-			self.DFSRecursivo(começo - 1, visitado)
+	def DFS(self, inicio):
+		visitado = [False] * self.v
+		pilha = [inicio - 1]
+		
+		while pilha:
+			vertice = pilha.pop()
+			
+			if not visitado[vertice]:
+				grafoUtils.printDFS(g, arquivoSaida, vertice)
+				visitado[vertice] = True
+				
+				for vizinho in range(self.v - 1, -1, -1):
+					if self.matrizAdj[vertice][vizinho] > 1 and not visitado[vizinho]:
+						pilha.append(vizinho)
 
 	def calcular_grau_medio(self):
-		grau_medio = self.arestas/self.V
-		
+		grau_medio = self.arestas/self.v
 		return grau_medio
 	
-				
 	def calcular_distribuicao_empirica(self):
 		distDistribuicao = {}
-		total_vertices = self.V
+		total_vertices = self.v
 		distribuicao_empirica = "" 
-		for v in range(self.V):
-				grau = sum(1 for peso in self.grafo[v] if peso > 0)
+		for v in range(self.v):
+				grau = sum(1 for peso in self.matrizAdj[v] if peso > 0)
 				if grau in distDistribuicao:
 					distDistribuicao[grau] += 1
 				else:
 					distDistribuicao[grau] = 1
 		distribuicao_ordenada = sorted(distDistribuicao.items(), key=lambda x: x[0])  # Ordena a distribuição
-		with open("saida.txt", 'a') as saida:
-				saida.write("\nDistribuição Empírica:\n")
+		with open(arquivoSaida, 'a') as saida:
 				for grau, frequencia in distribuicao_ordenada:
 					distr = frequencia / total_vertices
 					distribuicao_empirica += "Grau {} ---> Distribuicao Empirica = {}\n".format(str(grau), str(distr))
 				return distribuicao_empirica
 
-
-
-
-	def saida (self):
-		with open('saida.txt', 'w') as saida:
-			saida.write('num de vertices: '+ str(self.V))
-			saida.write('\nnum de arestas: '+  str(self.arestas))
-			saida.write('\nGrau medio: '+  str(self.calcular_grau_medio())+'\n\n')
-			distribuicao_empirica = self.calcular_distribuicao_empirica()
-			saida.write(distribuicao_empirica)
-		saida.close()
 if __name__ == '__main__':
-	g = Grafo(0)
-	g = g.lerGrafo('grafoSimples.txt')
-	g.prim()
-	g.BFS(1)
-	g.DFS(1)
-	g.saida()
+	print ("\nOlá, professor Glauco! Bem-vindo à nossa biblioteca de grafos.")
+	print("Todas as respostas estarão armazenada em um txt chamado: nome do arquivo + respostas \n")
 	
-	print("\n")
+	arquivo = input("Insira o nome do arquivo que contém o grafo a ser lido: ")
+	arquivoSaida = arquivo
+	arquivoSaida = arquivoSaida.replace('.txt', '_respostas.txt')
+	g = Grafo(0)
+	g = grafoUtils.lerGrafo(g, arquivo)
+
+	while True:
+		print ("\nMenu:")
+		print ("1 - Dados do Grafo e Distribuição Empírica")
+		print ("2 - BFS")
+		print ("3 - DFS")
+		print ("4 - Componentes Conexas")
+		print ("5 - Distância e Caminho Mínimo em um Par de Vértices")
+		print ("6 - MST por Prim")
+		print ("7 - Distância Média")
+		print ("8 - Todas as operações acima")
+		print ("0 - Sair")
+
+		option = input("\nDigite a operação que você deseja realizar: ")
+		
+		match option:	
+			case "1":
+				grafoUtils.printDadosGrafo(g, arquivoSaida)
+				print("\nOperação realizada com sucesso!")
+
+			case "2":
+				with open(arquivoSaida, 'a') as BFS:
+					BFS.write("\nBFS:\n")
+				BFS.close()
+				g.BFS(1)
+				print("\nOperação realizada com sucesso!")
+
+			case "3":
+				with open(arquivoSaida, 'a') as DFS:
+					DFS.write("\n\nDFS:\n")
+				DFS.close()
+				g.DFS(1)
+				print("\nOperação realizada com sucesso!")
+
+			case "6":
+				g.prim()
+				print("\nOperação realizada com sucesso!")
+
+			case "8":
+				grafoUtils.printDadosGrafo(g, arquivoSaida)
+
+				with open(arquivoSaida, 'a') as BFS:
+					BFS.write("\nBFS:\n")
+				BFS.close()
+				g.BFS(1)
+
+				with open(arquivoSaida, 'a') as DFS:
+					DFS.write("\n\nDFS:\n")
+				DFS.close()
+				g.DFS(1)
+
+				g.prim()
+				print("\nOperação realizada com sucesso!")
+
+			case _:
+				sys.exit("Programa finalizado.")
---
+++ grafoUtils.py
@@ -0,0 +1,61 @@
+import grafo 
+
+# Função que faz a leitura de um grafo em txt
+def lerGrafo(g, arquivo):
+	with open(arquivo, 'r') as f:
+		linhas = f.readlines()
+	# Lê o número de vértices
+	vertices = int(linhas[0].strip())
+	# Cria matriz de adjacência para o grafo no txt
+	matrizAdj = [[0] * vertices for _ in range(vertices)]
+	for linha in linhas[1:]:
+		valores = linha.strip().split()
+		origem, destino, peso = int(valores[0]), int(valores[1]), int(valores[2])
+		matrizAdj[origem - 1][destino - 1] = peso
+		matrizAdj[destino - 1][origem - 1] = peso
+		
+	# Cria um grafo com as informações de grafo.txt
+	g.v = vertices
+	g.matrizAdj = matrizAdj
+	g.arestas = (len(linhas)-1)
+	return g
+
+def printMST(g, arquivoSaida, pai):
+		with open(arquivoSaida, 'a') as mst:
+			mst.write('\n\nMST: \n')
+			mst.write('Aresta/Peso \n')
+			pesoTotal = 0
+			for i in range(1, g.v):
+				pesoTotal = pesoTotal + g.matrizAdj[i][pai[i]]
+				mst.write(str(pai[i] + 1))
+				mst.write(' ')
+				mst.write(str(i + 1))
+				mst.write(' ')
+				mst.write(str(g.matrizAdj[i][pai[i]]))
+				mst.write('\n')
+			mst.write('Peso total: ')
+			mst.write(str(pesoTotal))
+			mst.write('\n')
+		mst.close()
+
+def printDadosGrafo (g, arquivoSaida):
+	with open(arquivoSaida, 'a') as saida:
+		saida.write('\nnum de vertices: '+ str(g.v))
+		saida.write('\nnum de arestas: '+  str(g.arestas))
+		saida.write('\nGrau medio: '+  str(g.calcular_grau_medio())+'\n\n')
+		saida.write("Distribuicao Empirica:\n")
+		distribuicao_empirica = g.calcular_distribuicao_empirica()
+		saida.write(distribuicao_empirica)
+	saida.close()
+
+def printBFS(g, arquivoSaida, vis):
+	with open(arquivoSaida, 'a') as BFS:
+		BFS.write(str(vis + 1))
+		BFS.write(' ')
+	BFS.close()
+
+def printDFS(g, arquivoSaida, vertice):
+	with open(arquivoSaida, 'a') as DFS:
+		DFS.write(str(vertice + 1))
+		DFS.write(' ')
+	DFS.close()
